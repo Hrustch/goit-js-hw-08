6850205457ce04926ce2f1059052797eaf424e7a
Hrustch <nik.stalker47@gmail.com>
2023-05-02 00:30:01 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const video = document.querySelector('#vimeo-player');
+const player = new Player(video);
+
+function saveTiming() {
+  player.getCurrentTime().then((seconds) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime).catch((error) => {
+    console.error(error);
+  });
+}
+
+const throttledSaveTiming = throttle(saveTiming, 1000);
+player.on('timeupdate', throttledSaveTiming);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
-
+import throttle from 'lodash.throttle';
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("[name='email']");
+const messageInput = document.querySelector("[name='message']");
+let email;
+let message;
+const throttledSaving = throttle(()=>{
+    email =  form.elements.email.value
+    message = form.elements.message.value
+    localStorage.setItem("feedback-form-state", JSON.stringify({email, message}));
+}, 500)
+form.addEventListener("input", throttledSaving)
+form.addEventListener("submit", (event)=>{
+    email =  form.elements.email.value
+    message = form.elements.message.value
+    event.preventDefault()
+    console.log({email, message})
+    localStorage.clear();
+    form.reset();
+    return;
+})
+const savedInfo = localStorage.getItem("feedback-form-state");
+if(savedInfo){
+    const {email, message} = JSON.parse(savedInfo)
+    emailInput.value = email
+    messageInput.value = message
+}
